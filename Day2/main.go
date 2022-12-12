eb2ab736dd799f39b6550420a4a154a8f38fd91b
Igor Wulff <igorwulff@gmail.com>
2022-12-12 20:22:03 +0100

    Day 2 - Part 2

--- Day2/main.go
+++ Day2/main.go
@@ -16,23 +16,41 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	points := 0
+	shape := ""
 
 	for scanner.Scan() {
 		result := strings.Split(scanner.Text(), " ")
 
-		switch result[1] {
-		case "X": // Rock X or A
-			points += 1
-		case "Y": // Paper Y or B
-			points += 2
-		case "Z": // Scizzors Z or C
+		// X = lose, Y = draw, Z = win
+		if result[1] == "Y" {
+			shape = result[0]
 			points += 3
+		} else if result[1] == "X" {
+			if result[0] == "A" {
+				shape = "C"
+			} else if result[0] == "B" {
+				shape = "A"
+			} else {
+				shape = "B"
+			}
+		} else if result[1] == "Z" {
+			if result[0] == "A" {
+				shape = "B"
+			} else if result[0] == "B" {
+				shape = "C"
+			} else {
+				shape = "A"
+			}
+			points += 6
 		}
 
-		if (result[0] == "A" && result[1] == "X") || (result[0] == "B" && result[1] == "Y") || (result[0] == "C" && result[1] == "Z") {
+		switch shape {
+		case "A": // Rock X or A
+			points += 1
+		case "B": // Paper Y or B
+			points += 2
+		case "C": // Scizzors Z or C
 			points += 3
-		} else if (result[0] == "A" && result[1] == "Y") || (result[0] == "B" && result[1] == "Z") || (result[0] == "C" && result[1] == "X") {
-			points += 6
 		}
 	}
 
