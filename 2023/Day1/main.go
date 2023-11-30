711cb813984d88e517226cae1eb26e9bda94eca0
Igor Wulff <i.wulff@youweagency.com>
2023-11-30 20:14:27 +0100

    Prep day 1

---
+++ 2023/Day1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+
+		} else {
+
+		}
+	}
+}
