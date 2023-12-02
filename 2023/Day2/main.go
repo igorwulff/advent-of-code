b7f69002873e3aa24ea3645780c0e3e030ff1c5f
Igor Wulff <igorwulff@gmail.com>
2023-12-02 07:57:24 +0100

    Day 2 Part 2

--- 2023/Day2/main.go
+++ 2023/Day2/main.go
@@ -20,37 +20,38 @@ func main() {
 	sum := 0
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), ":")
-		game, _ := strconv.Atoi(strings.Split(line[0], "Game ")[1])
+		//game, _ := strconv.Atoi(strings.Split(line[0], "Game ")[1])
 		sets := strings.Split(line[1], ";")
 
+		blue := 0
+		red := 0
+		green := 0
 		for _, set := range sets {
 			parts := strings.Split(set, ",")
 
-			blue := 0
-			red := 0
-			green := 0
 			for _, part := range parts {
 				pair := strings.Split(strings.TrimSpace(part), " ")
 
 				if strings.Contains(pair[1], "blue") {
 					color, _ := strconv.Atoi(pair[0])
-					blue += color
+					if color > blue {
+						blue = color
+					}
 				} else if strings.Contains(pair[1], "red") {
 					color, _ := strconv.Atoi(pair[0])
-					red += color
+					if color > red {
+						red = color
+					}
 				} else if strings.Contains(pair[1], "green") {
 					color, _ := strconv.Atoi(pair[0])
-					green += color
+					if color > green {
+						green = color
+					}
 				}
-			}
-
-			if blue > 14 || red > 12 || green > 13 {
-				game = 0
 			}
 		}
 
-		fmt.Println(game)
-		sum += game
+		sum += blue * red * green
 	}
 
 	fmt.Println(sum)
