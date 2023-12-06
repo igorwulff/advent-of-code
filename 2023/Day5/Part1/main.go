ba21f8278de9bb102c8df21682d6f28a2cd90a07
Igor Wulff <igorwulff@gmail.com>
2023-12-06 21:44:54 +0100

    Day 5 Part 1

--- 2023/Day5/Part1/main.go
+++ 2023/Day5/Part1/main.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("../sample.txt")
+	file, err := os.Open("../input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -16,22 +17,26 @@ func main() {
 
 	//count := 0
 	scanner := bufio.NewScanner(file)
-	
-	step := 0
-	map := make([][]string, 10)
-	for i := range map {
-		map[i] = make([]string, 3)
-	}
 
+	step := 0
+	//set := make([][]int, 10)
+	set := make(map[int][][]int)
+
+	var data []string
 	if scanner.Scan() {
 		// Map string of seeds to seeds slice
-		data := strings.Split(scanner.Text(), ":")
+		data = strings.Split(scanner.Text(), ": ")
 		data = strings.Split(data[1], " ")
-		seeds := make([]string, len(data))
-		seeds = append(seeds, data...)
 
 		// Skip next line.
 		scanner.Scan()
+		scanner.Scan()
+	}
+
+	seeds := make([]int, len(data))
+	for k, i := range data {
+		seed, _ := strconv.Atoi(i)
+		seeds[k] = seed
 	}
 
 	for scanner.Scan() {
@@ -39,13 +44,44 @@ func main() {
 			step++
 			// Skip next line.
 			scanner.Scan()
+
 		} else {
-			data := strings.Split(, scanner.Text())
-			
+			var entries []int
+			data := strings.Split(scanner.Text(), " ")
+			for _, i := range data {
+				entry, _ := strconv.Atoi(i)
+				entries = append(entries, entry)
+			}
+			set[step] = append(set[step], entries)
+		}
+	}
+	fmt.Println("==========")
+	sum := 0
+	for _, seed := range seeds {
+		var value = seed
+		fmt.Print("Seed:")
+		fmt.Println(seed)
+		for i := 0; i < len(set); i++ {
+			nodes := set[i]
+			fmt.Println(nodes)
+			for _, node := range nodes {
+				if value >= node[1] && value < node[1]+node[2] {
+					fmt.Println(value - node[1] + node[0])
+					value = value - node[1] + node[0]
+					break
+				}
+			}
+			fmt.Println(value)
+		}
+
+		fmt.Print("Location: ")
+		fmt.Println(value)
+
+		if value < sum || sum == 0 {
+			sum = value
 		}
 	}
 
-	sum := 0
+	fmt.Println(sum)
 
-	fmt.Println("The answer is: `" + fmt.Sprint(sum) + "`")
 }
