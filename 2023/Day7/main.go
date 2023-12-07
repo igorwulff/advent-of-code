d3dafdd58934230d995e3d4e8674134b8fd257d6
Igor Wulff <igorwulff@gmail.com>
2023-12-07 21:02:27 +0100

    Bump day 7

--- 2023/Day7/main.go
+++ 2023/Day7/main.go
@@ -11,6 +11,8 @@ import (
 	"time"
 )
 
+var strength = map[string]int{"A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
+
 func main() {
 	start := time.Now()
 
@@ -22,59 +24,66 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	strength := map[string]int{"A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
 	sum := 0
-	sets := make(map[string]int) // "32T3K" => 765
-
+	sets := make(map[string]int)   // "32T3K" => 765
+	values := make(map[string]int) // "32T3K" => 765
 	for scanner.Scan() {
 		data := strings.Split(scanner.Text(), " ")
 		value, _ := strconv.Atoi(data[1])
 		sets[data[0]] = value
-
-	}
-	setKeys := make([]string, 0, len(sets))
-
-	for key := range sets {
-		setKeys = append(setKeys, key)
-
-		matches := make(map[string]int)
-		for _, v := range key {
-			matches[string(v)]++
-		}
-
-		// Sort map by value.
-		keys := make([]string, 0, len(matches))
-		for v2 := range matches {
-			keys = append(keys, v2)
-		}
-
-		sort.SliceStable(keys, func(i int, j int) bool {
-			if matches[keys[i]] == matches[keys[j]] {
-				return strength[keys[i]] > strength[keys[j]]
-			}
-
-			return matches[keys[i]] > matches[keys[j]]
-		})
-		// End of sorting.
-
-		for _, v := range keys {
-			fmt.Print(v + ":")
-			fmt.Println(matches[v])
-		}
-
-		fmt.Println("==============")
+		values[data[0]] = 0
 	}
 
+	keys := make([]string, 0, len(sets))
+	for v2 := range sets {
+		keys = append(keys, v2)
+	}
+	fmt.Println(keys)
 	sort.SliceStable(keys, func(i int, j int) bool {
-		if matches[keys[i]] == matches[keys[j]] {
-			return strength[keys[i]] > strength[keys[j]]
-		}
+		sortl, valuesl := sortHand(keys[i], keys, sets)
+		sortr, valuesr := sortHand(keys[j], keys, sets)
 
-		return matches[keys[i]] > matches[keys[j]]
+		fmt.Println("CHECK:")
+		fmt.Println(sortl)
+		fmt.Println(valuesl)
+		fmt.Println(sortr)
+		fmt.Println(valuesr)
+
+		for _, i := range sortl {
+			for _, j := range sortr {
+				fmt.Println(valuesl[i])
+				fmt.Println(valuesr[j])
+
+			}
+		}
+		return true
 	})
 
+	fmt.Println("By name:", keys)
 	fmt.Println("The answer is: `" + fmt.Sprint(sum) + "`")
 
 	elapsed := time.Since(start)
 	log.Printf("Execution time: %s", elapsed)
+}
+
+func sortHand(hand string, keys []string, sets map[string]int) ([]string, map[string]int) {
+	matches := make(map[string]int)
+	for _, v := range hand {
+		matches[string(v)]++
+	}
+
+	// Sort map by value.
+	cards := make([]string, 0, len(matches))
+	for v := range matches {
+		cards = append(cards, v)
+	}
+
+	sort.SliceStable(keys, func(i int, j int) bool {
+		if matches[keys[i]] == matches[keys[j]] {
+			return strength[keys[i]] < strength[keys[j]]
+		}
+		return matches[keys[i]] < matches[keys[j]]
+	})
+
+	return cards, matches
 }
