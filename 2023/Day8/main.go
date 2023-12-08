442906aacdd5ae3a1f2d4063e140c92ebe6b1283
Igor Wulff <igorwulff@gmail.com>
2023-12-08 20:39:08 +0100

    Update

--- 2023/Day8/main.go
+++ 2023/Day8/main.go
@@ -53,37 +53,37 @@ func main() {
 	}
 
 	steps := 0
+	coordLength := len(coord);
 out:
 	for {
-		for _, direction := range coord {
+		for i:=0; i<coordLength; i++ {
 			steps++
-			newNodes := make([]*node, 0)
-			for _, node := range startNodes {
-				if string(direction) == "L" {
-					newNodes = append(newNodes, node.left)
-				} else {
-					newNodes = append(newNodes, node.right)
-				}
-			}
-			startNodes = newNodes
-
-			matches := 0
-
-			//fmt.Printf("Run %d", steps)
-			for _, node := range startNodes {
-				if string(node.value[2]) == "Z" {
-					matches++
-				}
+			if coord[i] == 76 { // L
+				startNodes[0] = startNodes[0].left
+				startNodes[1] = startNodes[1].left
+				startNodes[2] = startNodes[2].left
+				startNodes[3] = startNodes[3].left
+				startNodes[4] = startNodes[4].left
+				startNodes[5] = startNodes[5].left
+			} else {
+				startNodes[0] = startNodes[0].right
+				startNodes[1] = startNodes[1].right
+				startNodes[2] = startNodes[2].right
+				startNodes[3] = startNodes[3].right
+				startNodes[4] = startNodes[4].right
+				startNodes[5] = startNodes[5].right
 			}
 
-			if matches == len(startNodes) {
+			if (startNodes[0].value[2] == 90 && startNodes[1].value[2] == 90 && startNodes[2].value[2] == 90 && startNodes[3].value[2] == 90 && startNodes[4].value[2] == 90 && startNodes[5].value[2] == 90) {
 				break out
 			}
 		}
+
+		fmt.Println(steps);
 	}
 
-	elapsed := time.Since(start)
-	log.Printf("Execution time: %s", elapsed)
+
+	log.Printf("Execution time: %s", time.Since(start))
 	fmt.Printf("Steps needed: %d", steps)
 
 	/// 21883 to low answer.
