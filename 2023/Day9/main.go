51f64dd27b9c513bf67d91db015e626f650ccd65
Igor Wulff <i.wulff@youweagency.com>
2023-12-09 11:44:54 +0100

    Day9 Part 1 +2

--- 2023/Day9/main.go
+++ 2023/Day9/main.go
@@ -21,32 +21,48 @@ type node struct {
 func main() {
 	start := time.Now()
 
-	file, err := os.Open("./sample.txt")
+	file, err := os.Open("./input.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	r, _ := regexp.Compile("([0-9A-Z]+)")
+	r, _ := regexp.Compile("([0-9A-Z-]+)")
+	seq := 0
+	for scanner.Scan() {
+		raw := r.FindAllString(scanner.Text(), -1)
+		data := make([]int, len(raw))
 
-	sum := 0
-	scanner.Scan()
-	raw := r.FindAllString(scanner.Text(), -1)
-	data := make([]int, len(raw))
-	for _, v := range raw {
-		value, _ := strconv.Atoi(v)
-		if value > 0 {
-			data = append(data, value)
+		for k, v := range raw {
+			value, _ := strconv.Atoi(v)
+			data[k] = value
 		}
+
+		h := history(data)
+		seq += h
 	}
-	fmt.Println(data)
+
+	fmt.Println("")
+	log.Printf("Execution time: %s", time.Since(start))
+	fmt.Printf("Next sequence: %d", seq)
+	fmt.Println("")
+}
+
+func history(data []int) int {
+	diff := make([]int, len(data)-1)
 
 	for i := 0; i < len(data)-1; i++ {
-		diff := data[i+1] - data[i]
-		fmt.Println(diff)
+		diff[i] = calcDiff(data[i], data[i+1])
 	}
 
-	log.Printf("Execution time: %s", time.Since(start))
-	fmt.Printf("Steps needed: %d", sum)
+	if len(diff) > 1 {
+		return history(diff) + data[0]
+	}
+
+	return data[0]
+}
+
+func calcDiff(a, b int) int {
+	return a - b
 }
