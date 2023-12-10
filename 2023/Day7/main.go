b38a7e2ee6c36fd4267de5464be130eab02f7984
Igor Wulff <i.wulff@youweagency.com>
2023-12-10 09:27:35 +0100

    Day 7 Part 2 Start

--- 2023/Day7/main.go
+++ 2023/Day7/main.go
@@ -11,12 +11,12 @@ import (
 	"time"
 )
 
-var strength = map[string]int{"A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
+var strength = map[string]int{"A": 14, "K": 13, "Q": 12, "J": 1, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
 
 func main() {
 	start := time.Now()
 
-	file, err := os.Open("./input.txt")
+	file, err := os.Open("./sample.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -82,21 +82,33 @@ func main() {
 }
 
 func handValue(hand map[rune]int) int {
+
 	value := 0 //XX.XX.XX.XX.XX.XX.XX
-	for _, v := range hand {
+
+	// Get Jokers.
+	jokers := 0
+	for k := range hand {
+		if string(k) == "J" {
+			jokers++
+		}
+	}
+
+	// Hand always has 5 cards.
+	for k, v := range hand {
+		fmt.Print(string(k) + ":")
+		fmt.Println(v)
 		// Five of a kind.
-		if v == 5 {
+		if v+jokers == 5 {
 			value = 7
 		}
 
 		// Four of a kind.
-		if v == 4 {
+		if v+jokers == 4 {
 			value = 6
-			continue
 		}
 
 		// Three of a kind
-		if v == 3 {
+		if v+jokers == 3 {
 			if value == 2 {
 				value = 5
 			} else {
@@ -109,7 +121,6 @@ func handValue(hand map[rune]int) int {
 				value = 5 // Full house... = 5
 			} else if value == 2 { //if value is already a pair.
 				value = 3 // 2 pairs... = 3
-				continue
 			} else {
 				value = 2
 			}
