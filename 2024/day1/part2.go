2dba30b2b13b2f188a9b326c6ac699d743597743
Igor Wulff <i.wulff@youweagency.com>
2024-12-02 11:34:58 +0100

    Added test case for day 1 part 2.

--- 2024/day1/part2.go
+++ 2024/day1/part2.go
@@ -10,6 +10,18 @@ import (
 func Part2(input string) string {
 	left, right := shared.ParseInput(input)
 
+	m := findOccurences(left, right)
+
+	output := 0
+	for _, l := range left {
+		output += l * m[l]
+
+	}
+
+	return fmt.Sprint(output)
+}
+
+func findOccurences(left, right []int) map[int]int {
 	m := make(map[int]int)
 
 	for _, l := range left {
@@ -24,10 +36,5 @@ func Part2(input string) string {
 		m[l] = i
 	}
 
-	output := 0
-	for k, v := range m {
-		output += k * v
-	}
-
-	return fmt.Sprint(output)
+	return m
 }
--- 2024/day1/part2_test.go
+++ 2024/day1/part2_test.go
@@ -5,15 +5,15 @@ import (
 )
 
 func TestPart2Sample(t *testing.T) {
-	/*input := `3   4
+	input := `3   4
 	4   3
 	2   5
 	1   3
 	3   9
 	3   3`
-	*/
+
 	expected := "31"
-	output := "31" //Part2(input)
+	output := Part2(input)
 
 	if output != expected {
 		t.Errorf("Expected %s, got %s", expected, output)
