a964150a90d4ec31879c8312938c5ad0ba21095c
Igor Wulff <i.wulff@youweagency.com>
2024-12-03 09:32:46 +0100

    Added Day 3 Part 1.

--- 2024/day3/part1.go
+++ 2024/day3/part1.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
 // Exported function to be called by the main application
 func Part1(input string) string {
-	return ""
+	r, err := regexp.Compile(`mul\(([0-9]+),([0-9]+)\)`)
+
+	if err != nil {
+		return "Regex can not be compiled."
+	}
+
+	matches := r.FindAllStringSubmatch(input, -1)
+
+	output := 0
+	for _, match := range matches {
+		l, _ := strconv.Atoi(match[1])
+		r, _ := strconv.Atoi(match[2])
+		output += l * r
+	}
+
+	return fmt.Sprint(output)
 }
--- 2024/day3/part1_test.go
+++ 2024/day3/part1_test.go
@@ -5,10 +5,12 @@ import (
 )
 
 func TestPart1Sample(t *testing.T) {
-	/*expected := "11"
+	input := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	expected := "161"
+
 	output := Part1(input)
 
 	if output != expected {
 		t.Errorf("Expected %s, got %s", expected, output)
-	}*/
+	}
 }
