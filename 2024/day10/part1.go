3fe11e9296fb4f2d28fab3446c1fd7198060cdc3
Igor Wulff <i.wulff@youweagency.com>
2024-12-10 15:37:26 +0100

    Add bad part 10.

--- 2024/day10/part1.go
+++ 2024/day10/part1.go
@@ -1,6 +1,105 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/igorwulff/advent-of-code/utils"
+)
+
+type Grid struct {
+	Width  int
+	Height int
+	Cells  []int
+	Starts []int
+}
+
+func (g Grid) InBounds(x, y int) bool {
+	return x >= 0 && x < g.Width && y >= 0 && y < g.Height
+}
+
+func (g *Grid) GetX(pos int) int {
+	return pos % g.Width
+}
+
+func (g *Grid) GetY(pos int) int {
+	return pos / g.Width
+}
+
+func (g Grid) GetXY(pos int) (int, int) {
+	return g.GetX(pos), g.GetY(pos)
+}
+
+func (g *Grid) GetPos(x, y int) int {
+	return (g.Width * y) + x
+}
+
+func (g *Grid) FindPath(pos int, ends *map[int]struct{}) {
+	x, y := g.GetXY(pos)
+	value := g.Cells[pos]
+
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			// Don't include diagonal values or center.
+			if utils.AbsInt(i) == utils.AbsInt(j) {
+				continue
+			}
+
+			xx := x + i
+			yy := y + j
+
+			if !g.InBounds(xx, yy) {
+				continue
+			}
+
+			next := g.GetPos(xx, yy)
+			if g.Cells[next] == 9 {
+				(*ends)[next] = struct{}{}
+			} else if g.Cells[next] == value+1 {
+				g.FindPath(next, ends)
+			}
+		}
+	}
+}
+
 // Exported function to be called by the main application
 func Part1(input string) string {
-	return ""
+	lines := strings.Split(input, "\n")
+
+	grid := Grid{
+		Width:  len(lines[0]),
+		Height: len(lines),
+		Cells:  make([]int, 0, len(lines[0])*len(lines)),
+		Starts: make([]int, 0),
+	}
+
+	for y, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		row := strings.Split(line, "")
+		for x, v := range row {
+			value, _ := strconv.Atoi(v)
+			grid.Cells = append(grid.Cells, value)
+
+			if value == 0 {
+				grid.Starts = append(grid.Starts, grid.GetPos(x, y))
+			}
+		}
+	}
+
+	sum := 0
+	for _, start := range grid.Starts {
+		ends := make(map[int]struct{}, 0)
+		grid.FindPath(start, &ends)
+
+		fmt.Println("X", grid.GetX(start), "Y", grid.GetY(start), "Lenght:", len(ends))
+
+		fmt.Println(ends)
+		sum += len(ends)
+	}
+
+	return fmt.Sprint(sum)
 }
--- 2024/day10/part1_test.go
+++ 2024/day10/part1_test.go
@@ -4,6 +4,37 @@ import (
 	"testing"
 )
 
+/*func TestPart1Simple(t *testing.T) {
+	input := `0123
+1234
+8765
+9876`
+
+	expected := "1"
+	output := Part1(input)
+
+	if output != expected {
+		t.Errorf("Expected %s, got %s", expected, output)
+	}
+}
+
+func TestPart1Simple2(t *testing.T) {
+	input := `2220222
+2221222
+2222222
+6543456
+7224227
+8225678
+9298729`
+
+	expected := "3"
+	output := Part1(input)
+
+	if output != expected {
+		t.Errorf("Expected %s, got %s", expected, output)
+	}
+}*/
+
 func TestPart1Sample(t *testing.T) {
 	input := `89010123
 78121874
